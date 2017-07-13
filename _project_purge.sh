c25a665468e935eac029f1fa2ed76346dcf2939d
Steve Stevens <steven.stevens@travisperkins.co.uk>
2017-07-13 14:11:34 +0100

    purge script is a bit more thorough

--- _project_purge.sh
+++ _project_purge.sh
@@ -6,7 +6,40 @@ if [ $EUID != 0 ]; then
   exit $?
 fi
 
-# Purge
-rm -rf **/bin **/test-output **/build **/target
-rm -rf **/cucumber.properties
+function deleteDirectories() {
+  declare -a dirs=( $(find . -type d -name "$1") )
+  for dir in ${dirs[@]}
+  do
+    echo "Removing dir: ${dir}"
+
+    # Force remove it from git
+    git rm -rf --ignore-unmatch -- ${dir} > /etc/null
+
+    # Force delete file from system
+    rm -rf ${dir} > /etc/null
+  done
+}
+
+function deleteFiles() {
+  declare -a files=( $(find . -name "$1") )
+  for file in ${files[@]}
+  do
+    echo "Removing file: ${file}"
+
+    # Force remove it from git
+    git rm -f --ignore-unmatch -- ${file} > /etc/null
+
+    # Force delete file from system
+    rm -f ${file} > /etc/null
+  done
+}
+
+# Purge Directories
+deleteDirectories "bin"
+deleteDirectories "build"
+deleteDirectories "test-output"
+deleteDirectories "target"
+
+# Purge files
+deleteFiles "cucumber.properties"
 
